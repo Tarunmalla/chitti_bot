bf37538b3995fcee856d3f7e3b7468df87f2c8be
vinay-0821 <cvinay2108@gmail.com>
2024-06-30 18:10:42 +0530

    solved confilcts

--- leetcode/prob_count.py
+++ leetcode/prob_count.py
@@ -1,12 +1,8 @@
 import requests
 import sqlite3
 import pandas as pd
-<<<<<<< HEAD
-from table2ascii import table2ascii as t2a,PresetStyle
-df=pd.DataFrame(columns=['Handle','ALL','Easy','Medium','Hard'])
-=======
+
 from table2ascii import table2ascii as t2a, PresetStyle
->>>>>>> 75e0d33a34f5640558091aded58792927c1b33c7
 
 def user_info():
     df=pd.DataFrame(columns=['Handle','ALL','Easy','Medium','Hard'])
@@ -44,22 +40,10 @@ def user_info():
                 handler_info.append(info)
         handler_info.insert(0,handle[0])
         df.loc[len(df.index)] = handler_info
-<<<<<<< HEAD
-    return df
-    # header=df.columns.tolist()
-    # body=df.values.tolist()
-    # output=t2a(
-    #     header=header,
-    #     body=body,
-    #     style=PresetStyle.think_compact
-    # )
-    # return output
-=======
-        
+               
     table = convert_to_table(df)
     # print(table)
     return table
->>>>>>> 75e0d33a34f5640558091aded58792927c1b33c7
 
 def convert_to_table(df):
     table = t2a(
